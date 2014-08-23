97bfb18a9f7aa8e6fbbc55ca8b7c60f1b3588131
Steven Hsu <admin@stevenhsu.ca>
2014-08-22 19:39:09 -0700

    Functions now pass tweet object as parameters. Moved twitter username from the message line into the title.

--- worker.rb
+++ worker.rb
@@ -9,38 +9,40 @@ require 'json'
 
 users_to_monitor = {
   33918567 => 'am730traffic',
-# 41618221 => 'drivebc',
   104470692 => 'news1130traffic',
   61617150 => 'translink' 
 }
 
-def isHighwayIncidents?(message)
-  if @highways_to_monitor.any? { |highway| message.downcase.include? highway }
-    priority = 1 if @events.any? { |event| message.downcase.include? event }
-    sendToPushover(message, priority)
+def isHighwayIncidents?(tweet)
+  msg = tweet.text.downcase
+  if @highways_to_monitor.any? { |highway| msg.include? highway }
+    priority = 1 if @events.any? { |event| msg.include? event }
+    sendTweetToPushover(tweet, priority)
     true
   end
 end
 
-def isSkytrainIncidents?(message)
-  if (message.downcase.include? 'skytrain') &&  
-    @events.any? { |event| message.downcase.include? event}
-     sendToPushover(message, 1)
+def isSkytrainIncidents?(tweet)
+  msg = tweet.text.downcase
+  if (msg.include? 'skytrain') &&  
+    @events.any? { |event| msg.include? event}
+     sendTweetToPushover(tweet, 1)
      true
   end
 end
 
-def sendToPushover(message, priority = -1)
+def sendTweetToPushover(tweet, priority = -1)
   pushoverResponse = HTTP.post('https://api.pushover.net/1/messages.json', params: {
     token: "#{ENV['PUSHOVER_APP_TOKEN']}",
     user: "#{ENV['PUSHOVER_USER_KEY']}",
-    message: message,
+    title: tweet.user.name,
+    message: tweet.text,
     priority: priority
   })
 
   if pushoverResponse.status_code == 200
     serverity = priority == 1 ? 'Warn':'Info'
-    puts "[#{serverity}] #{message}"
+    puts "[#{serverity}] #{tweet.user.name} #{tweet.text}"
   elsif pushoverResponse.status_code == 400
     puts "[Error] The " + JSON.parse(pushoverResponse.to_s)['errors'].join(' and ')
   else
@@ -57,13 +59,12 @@ client = Twitter::Streaming::Client.new({
   access_token_secret: "#{ENV['TWITTER_ACCESS_TOKEN_SECRET']}"
 })
 
-client.filter(follow: users_to_monitor.keys.join(', ')) do |object|
-  if object.is_a?(Twitter::Tweet) && object.in_reply_to_user_id.nil? && 
-    users_to_monitor.has_key?(object.user.id)
+client.filter(follow: users_to_monitor.keys.join(', ')) do |tweet|
+  if tweet.is_a?(Twitter::Tweet) && tweet.in_reply_to_user_id.nil? && 
+    users_to_monitor.has_key?(tweet.user.id)
 
-    message = "#{object.user.name}: #{object.text}"
-    if !isHighwayIncidents?(message) && !isSkytrainIncidents?(message) && ENV['DEBUG']
-      puts "[Debug] #{message}"
+    if !isHighwayIncidents?(tweet) && !isSkytrainIncidents?(tweet) && ENV['DEBUG']
+      puts "[Debug] #{tweet.user.name}: #{tweet.text}"
     end
   end
 end
